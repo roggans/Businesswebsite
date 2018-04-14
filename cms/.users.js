f4c3292a9252529a65190e183c4ed9671fe152bf
Unknown <rogge_acdc@hotmail.com>
2018-04-15 00:46:35 +0200

    setup

---
+++ cms/.users.js
@@ -0,0 +1,11 @@
+({
+	users: [
+		{
+			username: 'admin',
+			tags: [],
+			salt: '8e6a8708ffe77ea99fca1e7f17d4166e',
+			hash: '5119f108c736c4b098d7e6c439c249eb40d4394d33d6c27d9df98ab031b4c1a2',
+			user_created_timestamp: 1523617251625
+		}
+	]
+})
